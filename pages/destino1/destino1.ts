e7736dba4026bc0c0c091f2572baadc4b204a7fd
paulaakemi <paulaakemi14@gmail.com>
2018-11-04 22:33:11 -0200

    Ionic06

---
+++ pages/destino1/destino1.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Genero } from '../../model/model_filme';
+
+/**
+ * Generated class for the Destino1Page page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-destino1',
+  templateUrl: 'destino1.html',
+})
+export class Destino1Page {
+
+  genero: Genero;
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.genero = this.navParams.get("generoSelecionado");
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Destino1Page');
+  }
+
+}
